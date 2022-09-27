10fd75959020830016766674b75162b2eabd6491
bruceeewong <292803281@qq.com>
2022-09-27 12:14:02 -0400

    build: improve bundling settings for ssg compatible

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,43 +3,46 @@ import typescript from '@rollup/plugin-typescript';
 import cjs2es from '@rollup/plugin-commonjs';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'rollup';
-import { babel } from '@rollup/plugin-babel';
+import {getBabelOutputPlugin} from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import * as path from 'path';
 
 const config = defineConfig({
   input: 'src/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'es',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      plugins: [
+        // compile js to es5 compatible, friendly to browsers
+        getBabelOutputPlugin({
+          filename: 'dist/index.js',
+          configFile: path.resolve(__dirname, 'babel.config.js'),
+        })
+      ]
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+    },
+  ],
   external: ['@mysten/sui.js'],
   plugins: [
+    // compile ts files
+    typescript(),
     // polyfill nodejs built-in and global modules
-    nodePolyfills({
-      // include: ['process']
-    }),
+    nodePolyfills(),
     // fetch node_modules contents
     resolvePlugin({
       browser: true, // specify that it's built for browser
     }),
-    // compile ts files
-    typescript({
-      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-    }),
     // convert commonjs module to es module for rollup to bundle
-    cjs2es(), // must place before babel
-    // compile js to es5 compatible, friendly to browsers
-    babel({
-      babelHelpers: 'bundled',
-      exclude: '**/node_modules/**',
-    }),
+    cjs2es(),
   ],
 });
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(terser()); // minify output files
+  config.plugins?.push(terser()); // minify output files
 }
 
 export default config;
